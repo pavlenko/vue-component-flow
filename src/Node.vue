a1ff757ba96bd18b85211bfefd9cd73491b64f0d
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2018-12-14 18:47:38 +0200

    init

---
+++ src/Node.vue
@@ -0,0 +1,15 @@
+<script>
+    module.exports = {
+        props: {
+            type: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            label: {
+                type: String
+            }
+        }
+    };
+</script>
