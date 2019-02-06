d795e1a9c84e8abdf3dcc67dbfad5a56f94344ec
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-02-06 13:48:38 +0200

    start build links

--- vue-flow.js
+++ vue-flow.js
@@ -2,6 +2,9 @@ var VueFlow = {
     components: {},
     utils: {
         snapTo: function (value, snap) { return Math.round(Math.round(value / snap) * snap); },
+        distance (x1, y1, x2, y2) {
+            return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+        },
         generateUUID: function () {
             var date = new Date().getTime();
 
@@ -13,6 +16,91 @@ var VueFlow = {
         }
     }
 };
+
+VueFlow.components['vue-flow-link'] = Vue.component('vue-flow-link', {
+    props: {
+        id: String,
+        sourceX: Number,
+        sourceY: Number,
+        targetX: Number,
+        targetY: Number,
+        sourceBlock: String,
+        sourcePort: String,
+        targetBlock: String,
+        targetPort: String
+    },
+    computed: {
+        /*pathes: function () {
+            let pathes = [1];
+
+            var sourceEL = this.sourcePort || this.sourceBlock;
+            var targetEL = this.targetPort || this.targetBlock;
+
+            this.$nextTick(function () {
+                var sourceBlock = this.$parent.$refs.blocks[this.$parent.blocks.findIndex(function (block) {
+                    return block.id == this.sourceBlock;
+                }.bind(this))];
+
+                console.log(sourceBlock);
+            });
+
+            if (this.sourceBlock) {
+                if (this.sourcePort) {
+                    //TODO find block port el x/y
+                } else {
+                    //TODO find block el x/y
+                }
+            } else {
+                var source = {x: this.sourceX, y: this.sourceY};
+            }
+
+            if (this.targetBlock) {
+                if (this.targetPort) {
+                    //TODO find block port el x/y
+                } else {
+                    //TODO find block el x/y
+                }
+            } else {
+                var target = {x: this.targetX, y: this.targetY};
+            }
+
+            /!*this.lines.forEach(l => {
+                let dist = this.distance(l.x1, l.y1, l.x2, l.y2) * 0.25
+                pathes.push({
+                    data: `M ${l.x1}, ${l.y1} C ${(l.x1 + dist)}, ${l.y1}, ${(l.x2 - dist)}, ${l.y2}, ${l.x2}, ${l.y2}`,
+                    style: l.style,
+                    outlineStyle: l.outlineStyle
+                })
+            })*!/
+
+            return pathes;
+        },*/
+        arrows: function () {
+            return [
+                {
+                    transform: 'translate(843.5, 237) rotate(-90)'
+                }
+            ];
+        }
+    },
+    template:
+        '<g v-for="p in getPathes()">' +
+        '    <path v-for="a in arrows" d="M -1 -1 L 0 1 L 1 -1 z" :transform="a.transform" stroke="rgb(255, 136, 85)" stroke-width="8" fill="rgb(255, 136, 85)"></path>' +
+        '</g>',
+    methods: {
+        getPathes: function () {
+            this.$nextTick(function () {
+                var sourceBlock = this.$parent.$refs.blocks[this.$parent.blocks.findIndex(function (block) {
+                    return block.id == this.sourceBlock;
+                }.bind(this))];
+
+                console.log(sourceBlock);
+            });
+
+            return [1];
+        }
+    }
+});
 
 VueFlow.components['vue-flow-block'] = Vue.component('vue-flow-block', {
     props: {
@@ -80,7 +168,7 @@ VueFlow.components['vue-flow-block'] = Vue.component('vue-flow-block', {
             this.$emit('remove');
         },
         onMouseDown: function (event) {
-            var rect = this.$parent.$el.getBoundingClientRect();
+            var rect = this.$el.getBoundingClientRect();
 
             this.cursorPositionX = event.pageX || event.clientX + document.documentElement.scrollLeft;
             this.cursorPositionY = event.pageY || event.clientY + document.documentElement.scrollTop;
@@ -149,8 +237,14 @@ VueFlow.components['vue-flow-paper'] = Vue.component('vue-flow-paper', {
         }
     },
     template:
-        '<div class="vf-paper" style="height: 500px">' +
-        '    <vue-flow-block v-for="block in blocks"' +
+        '<div class="vf-paper" style="height: 200px">' +
+        '    <svg width="100%" height="100%">' +
+        '        <vue-flow-link v-for="link in links"' +
+        '                       :key="link.id"' +
+        '                       v-bind.sync="link"/>' +
+        '    </svg>' +
+        '    <vue-flow-block ref="blocks"' +
+        '                    v-for="block in blocks"' +
         '                    :key="block.id"' +
         '                    v-bind.sync="block"' +
         '                    @select="selectBlock(block)"' +
